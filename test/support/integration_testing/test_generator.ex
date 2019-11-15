40861b183438131cf783d32603cf9326982740d3
Aaron Renner <aaron@renner.live>
2019-11-14 23:08:38 -0700

    Fix TestGenerator for passed in scenarios

--- test/support/integration_testing/test_generator.ex
+++ test/support/integration_testing/test_generator.ex
@@ -5,37 +5,31 @@ defmodule WebDriverClient.IntegrationTesting.TestGenerator do
   alias WebDriverClient.IntegrationTesting.Scenarios.Scenario
 
   defmacro generate_describe_per_scenario(opts \\ [], do: block) when is_list(opts) do
-    scenarios =
-      case Keyword.fetch(opts, :scenarios) do
-        {:ok, scenarios_ast} ->
-          {scenarios, _} = Code.eval_quoted(scenarios_ast, [], __CALLER__)
-          scenarios
+    default_scenarios = Scenarios.all()
 
-        :error ->
-          Scenarios.all()
-      end
+    quote do
+      unquote(opts)
+      |> Keyword.get(:scenarios, unquote(Macro.escape(default_scenarios)))
+      |> Enum.each(fn scenario ->
+        %Scenario{
+          driver: driver,
+          browser: browser,
+          session_configuration_name: configuration_name
+        } = scenario
 
-    Enum.map(scenarios, fn scenario ->
-      %Scenario{
-        driver: driver,
-        browser: browser,
-        session_configuration_name: configuration_name
-      } = scenario
+        integration_test_driver_configuration_name = "#{driver}-#{configuration_name}"
+        integration_test_driver_browser = "#{driver}-#{browser}"
 
-      integration_test_driver_configuration_name = "#{driver}-#{configuration_name}"
-      integration_test_driver_browser = "#{driver}-#{browser}"
-
-      quote do
-        describe unquote(inspect(scenario)) do
-          @describetag scenario: unquote(Macro.escape(scenario))
-          @describetag integration_test_driver: unquote(to_string(driver))
-          @describetag integration_test_driver_browser: unquote(integration_test_driver_browser)
+        describe inspect(scenario) do
+          @describetag scenario: scenario
+          @describetag integration_test_driver: to_string(driver)
+          @describetag integration_test_driver_browser: integration_test_driver_browser
           @describetag integration_test_driver_configuration_name:
-                         unquote(integration_test_driver_configuration_name)
+                         integration_test_driver_configuration_name
 
           unquote(block)
         end
-      end
-    end)
+      end)
+    end
   end
 end
