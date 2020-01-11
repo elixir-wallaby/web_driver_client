dd5fee5390eea4a727e96771fb56fe3c93db4fc1
Aaron Renner <aaron@renner.live>
2020-01-11 00:15:25 -0700

    Remove unused JWP middleware

--- lib/web_driver_client/json_wire_protocol_client/tesla_client_builder.ex
+++ lib/web_driver_client/json_wire_protocol_client/tesla_client_builder.ex
@@ -3,28 +3,6 @@ defmodule WebDriverClient.JSONWireProtocolClient.TeslaClientBuilder do
   alias Tesla.Client
   alias WebDriverClient.Config
 
-  @spec build(Config.t()) :: Client.t()
-  def build(%Config{base_url: base_url, debug?: debug?}) do
-    adapter = {Tesla.Adapter.Hackney, [recv_timeout: 30_000]}
-
-    middleware =
-      [
-        {Tesla.Middleware.BaseUrl, base_url},
-        WebDriverClient.JSONWireProtocolClient.Middleware.UnsucessfulStatusHandler,
-        WebDriverClient.JSONWireProtocolClient.Middleware.APIResponseHandler,
-        WebDriverClient.JSONWireProtocolClient.Middleware.ParseJSON,
-        Tesla.Middleware.EncodeJson,
-        WebDriverClient.Middleware.HTTPClientErrorHandler,
-        Tesla.Middleware.Logger
-      ]
-      |> Enum.reject(fn
-        Tesla.Middleware.Logger -> !debug?
-        _ -> false
-      end)
-
-    Tesla.client(middleware, adapter)
-  end
-
   @spec build_simple(Config.t()) :: Client.t()
   def build_simple(%Config{base_url: base_url, debug?: debug?}) do
     adapter = {Tesla.Adapter.Hackney, [recv_timeout: 30_000]}
