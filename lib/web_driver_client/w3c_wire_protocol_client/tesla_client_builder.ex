c2df90c7b507cf3b16e475569d5e0fe568c31f65
Aaron Renner <aaron@renner.live>
2020-01-15 19:13:46 -0700

    Remove unused W3C middleware

--- lib/web_driver_client/w3c_wire_protocol_client/tesla_client_builder.ex
+++ lib/web_driver_client/w3c_wire_protocol_client/tesla_client_builder.ex
@@ -3,27 +3,6 @@ defmodule WebDriverClient.W3CWireProtocolClient.TeslaClientBuilder do
   alias Tesla.Client
   alias WebDriverClient.Config
 
-  @spec build(Config.t()) :: Client.t()
-  def build(%Config{base_url: base_url, debug?: debug?}) do
-    adapter = {Tesla.Adapter.Hackney, [recv_timeout: 30_000]}
-
-    middleware =
-      [
-        {Tesla.Middleware.BaseUrl, base_url},
-        WebDriverClient.W3CWireProtocolClient.Middleware.ErrorResponseHandler,
-        WebDriverClient.W3CWireProtocolClient.Middleware.ParseJSON,
-        Tesla.Middleware.EncodeJson,
-        WebDriverClient.Middleware.HTTPClientErrorHandler,
-        Tesla.Middleware.Logger
-      ]
-      |> Enum.reject(fn
-        Tesla.Middleware.Logger -> !debug?
-        _ -> false
-      end)
-
-    Tesla.client(middleware, adapter)
-  end
-
   @spec build_simple(Config.t()) :: Client.t()
   def build_simple(%Config{base_url: base_url, debug?: debug?}) do
     adapter = {Tesla.Adapter.Hackney, [recv_timeout: 30_000]}
