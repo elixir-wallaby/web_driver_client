66d7f94e5baf7064083c420f4bc2be898103c118
Aaron Renner <aaron.renner@gmail.com>
2020-02-05 08:59:29 -0700

    Test logging across all scenarios
    Also adds an uncaught js error to test page so we can see the log output
    in each driver

--- test/integration/logging_test.exs
+++ test/integration/logging_test.exs
@@ -6,24 +6,14 @@ defmodule WebDriverClient.Integration.LoggingTest do
   alias WebDriverClient.IntegrationTesting.TestGenerator
   alias WebDriverClient.IntegrationTesting.TestPages.LoggingPage
   alias WebDriverClient.LogEntry
+  alias WebDriverClient.WebDriverError
 
   require WebDriverClient.IntegrationTesting.TestGenerator
 
   @moduletag :capture_log
   @moduletag :integration
 
-  scenarios =
-    Scenarios.all()
-    |> Enum.filter(fn
-      %Scenario{driver: :chromedriver, protocol: :w3c} ->
-        # Chromedriver does not allow for jwp endpoints in w3c mode
-        false
-
-      _ ->
-        true
-    end)
-
-  TestGenerator.generate_describe_per_scenario scenarios: scenarios do
+  TestGenerator.generate_describe_per_scenario do
     test "retrieving logs", %{scenario: scenario} do
       config = Scenarios.get_config(scenario)
       payload = Scenarios.get_start_session_payload(scenario)
@@ -32,12 +22,25 @@ defmodule WebDriverClient.Integration.LoggingTest do
 
       :ok = WebDriverClient.navigate_to(session, LoggingPage.url())
 
-      {:ok, log_types} = WebDriverClient.fetch_log_types(session)
-      assert Enum.all?(log_types, &is_binary/1)
+      case scenario do
+        %Scenario{driver: :chromedriver, protocol: :w3c} ->
+          # Chromedriver does not allow for fetching log types in w3c mode
+          assert {:error, %WebDriverError{reason: :unknown_command}} =
+                   WebDriverClient.fetch_log_types(session)
 
-      for log_type <- log_types do
-        {:ok, logs} = WebDriverClient.fetch_logs(session, log_type)
-        assert Enum.all?(logs, &match?(%LogEntry{}, &1))
+          for log_type <- ["browser", "driver"] do
+            {:ok, logs} = WebDriverClient.fetch_logs(session, log_type)
+            assert Enum.all?(logs, &match?(%LogEntry{}, &1))
+          end
+
+        %Scenario{} ->
+          {:ok, log_types} = WebDriverClient.fetch_log_types(session)
+          assert Enum.all?(log_types, &is_binary/1)
+
+          for log_type <- log_types do
+            {:ok, logs} = WebDriverClient.fetch_logs(session, log_type)
+            assert Enum.all?(logs, &match?(%LogEntry{}, &1))
+          end
       end
     end
   end
